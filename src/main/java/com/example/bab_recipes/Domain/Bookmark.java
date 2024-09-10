c8b7e459eed79a71e5e230ef4e596096b1be733e
kiseok <kisunk4546@gmail.com>
2024-09-10 16:39:15 +0900

    북마크 불러오기 수정(중복검사 user + RecipeID)

--- src/main/java/com/example/bab_recipes/Domain/Bookmark.java
+++ src/main/java/com/example/bab_recipes/Domain/Bookmark.java
@@ -14,6 +14,7 @@ public class Bookmark {
     private Long bookmarkId;
 
     //fetch = FetchType -> 지연로딩
+    @Getter
     @ManyToOne(fetch = FetchType.LAZY) //Many = bookmark, one = user ->한명의 유저가 많은 북마크를 남길수 있다.
     @JoinColumn(name="userId") //foreign key(userId) reference User (UserId)
     private User user;
@@ -21,7 +22,7 @@ public class Bookmark {
     //mongo
     @Getter
     @Setter
-    @Column(name = "recipeId", unique = true)
+    @Column(name = "recipeId", unique = false)
     private String recipeId;
 
     @Getter
